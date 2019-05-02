4e1f220e7fcaf17cd45fe1cf3dcc3f1e8cccbd32
Barth <juniior.barth@gmail.com>
2019-05-01 21:48:05 -0300

    Create method for insert ints in linked list

--- datastructs/linked_list.py
+++ datastructs/linked_list.py
@@ -10,3 +10,11 @@ class ListNode:
 class LinkedList:
     def __init__(self, val: Union[int, List] = None) -> None:
         self.head = self.tail = None
+        if val:
+            self.add(val)
+
+    def add(self, val):
+        if self.head is None:
+            self.head = self.tail = ListNode(val)
+        else:
+            self.tail.next = ListNode(val)
