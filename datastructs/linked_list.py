5f4b7810dd20e12c8a67382d650737a9e4e2ddaf
Barth <juniior.barth@gmail.com>
2019-05-01 21:49:58 -0300

    Create method to traverse the linked list

--- datastructs/linked_list.py
+++ datastructs/linked_list.py
@@ -18,3 +18,13 @@ class LinkedList:
             self.head = self.tail = ListNode(val)
         else:
             self.tail.next = ListNode(val)
+
+    def _traversal(self, list_node: ListNode = None) -> List:
+        res = []
+        if list_node is None:
+            return res
+        while list_node:
+            if list_node:
+                res.append(list_node.val)
+            list_node = list_node.next
+        return res
